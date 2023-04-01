96fdcf7f8bc3f5743f48578b8cf7658241f8b281
pcbzmani <pcbzmani@gmail.com>
2023-04-01 07:51:00 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name = "data_governance",
+    version = "0.1",
+    author = "Manikandan Muthiah",
+    author_email = "pcbzmani@gmail.com",
+    py_modules = ['data_gov'],
+    python_requires = '>3.6'
+)
